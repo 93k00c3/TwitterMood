b4a14bc596df226c2cca1c5c82dc9e9594be1842
93k00c3 <82811885+93k00c3@users.noreply.github.com>
2023-06-15 21:45:51 +0200

    preprocess_tweet method created

--- service/twitter_service.py
+++ service/twitter_service.py
@@ -1,3 +1,5 @@
+import nltk
+from nltk.corpus import stopwords
 import pandas as pd
 import re
 import langid
@@ -9,3 +11,14 @@ def filter_tweets(csv_file):
     df = df[df['Tweet'].apply(lambda x: langid.classify(x)[0] == 'en')]
     df.dropna(subset=['Tweet'], inplace=True)
     return df['Tweet']
+
+
+def preprocess_tweet(Tweet):
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
+    Tweet = re.sub(r'[^a-zA-Z\s]', '', Tweet)  # Remove special characters
+    tweet_tokens = nltk.word_tokenize(Tweet)
+    filtered_tokens = [word for word in tweet_tokens if word.lower() not in stop_words]  # Remove stopwords
+    filtered_tweet = ' '.join(filtered_tokens)
+    return filtered_tweet
+
